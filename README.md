b1c0f505a10202c807b877177e25b336f11f7654
Joep Meindertsma <joep@ontola.io>
2022-03-25 11:41:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Solid Search for Community Solid Server
+
+This is an extension / plugin for the Community Solid Server.
+It adds full-text search to the Community Solid Server.
+
+Limitations:
+
+- Only searches inside individual triples, not at resource level
+- Does not support named graphs or blank nodes
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {getLoggerFor, ResourceStore, ResourceIdentifier} from '@solid/community-server';
+import fetch from 'node-fetch';
+import { EventEmitter } from 'ws';
+
+/**
+ * Sends turtle files to the Search indexer endpoint whenever a resource is updated, which allows for full-text search.
+ */
+ export class SearchListener {
+  private readonly logger = getLoggerFor(this);
+
+  public constructor(source: EventEmitter, store: ResourceStore, searchEndpoint: string) {
+    // super();
+    // Every time a resource is changed, post to the Solid-Search instance
+    source.on('changed', async(changed: ResourceIdentifier): Promise<void> => {
+      const turtleStream = (await store.getRepresentation(changed, { type: { 'text/turtle': 1 } })).data;
+
+      const response = await fetch(searchEndpoint, {
+        method: "POST",
+        body: turtleStream,
+      });
+
+      if (response.status !== 200) {
+        this.logger.error(`Failed to post resource to search endpoint: ${response.status}`);
+      }
+    });
+  }
+}
